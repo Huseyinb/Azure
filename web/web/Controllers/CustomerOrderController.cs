4b13267c50c916773b520d3f0d26f7a42cdfb81e
Huseyinb <huseyinberber20@gmail.com>
2018-10-08 12:19:23 +0300

    azure web project
    azure web project

---
+++ web/web/Controllers/CustomerOrderController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using web.Models;
+
+namespace web.Controllers
+{
+    public class CustomerOrderController : Controller
+    {
+        private huseyinEntities2 db = new huseyinEntities2();
+        // GET: CustomerOrader
+        public ActionResult Index()
+        {
+            List<Customer> customerlist = db.Customers.ToList();
+            List<Order> orederlist = db.Orders.ToList();
+            List<Order_Detail> odlist = db.Order_Details.ToList();
+
+            return View();
+        }
+    }
+}
---
+++ web/web/Controllers/HomeController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+        public ActionResult Kayit()
+        {
+           
+            return View();
+        }
+        public ActionResult Giris()
+        {
+            return View();
+        }
+        public ActionResult Alerts()
+        {
+            return View();
+        }
+        public ActionResult Badge()
+        {
+            return View();
+        }
+        public ActionResult Buttons()
+        {
+            return View();
+        }
+        public ActionResult ButtonGroup()
+        {
+            return View();
+        }
+        public ActionResult Cards()
+        {
+            return View();
+        }
+        public ActionResult Carousel()
+        {
+            return View();
+        }
+        public ActionResult Collapse()
+        {
+            return View();
+        }
+        public ActionResult DropDowns()
+        {
+            return View();
+        }
+        public ActionResult Forms()
+        {
+            return View();
+        }
+        public ActionResult InputTypes()
+        {
+            return View();
+        }
+        public ActionResult Jumbotron()
+        {
+            return View();
+        }
+        public ActionResult ListGroup()
+        {
+            return View();
+        }
+        public ActionResult Modal()
+        {
+            return View();
+        }
+        public ActionResult Navs()
+        {
+            return View();
+        }
+        public ActionResult Pegination()
+        {
+            return View();
+        }
+        public ActionResult Popovers()
+        {
+            return View();
+        }
+        public ActionResult Progress()
+        {
+            return View();
+        }
+        public ActionResult Scrollspy()
+        {
+            return View();
+        }
+        public ActionResult Tooltips()
+        {
+            return View();
+        }
+    }
+}
